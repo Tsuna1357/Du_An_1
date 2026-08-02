7876f0b2974a678ee32a42ef81bad8c596288c04
Tsuna1357 <tsuna1357911@gmail.com>
2026-08-02 17:45:25 +0700

    create default script fuck conflig

---
+++ DUAN1/Assets/Script/FocusDefault.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class DefaultFocus : Interactable
+{
+    public CameraMap1 cameraMap;
+    public Transform focusPoint;
+
+    public bool isFocused = false;
+    public override void OnClick()
+    {
+        // Nếu chưa focus thì focus trước
+        if (!isFocused)
+        {
+            FocusIn();
+            return;
+        }
+
+        // Đã focus rồi
+        Debug.Log("Đã focus vào tủ");
+    }
+
+    public void FocusIn()
+    {
+        if (isFocused)
+            return;
+
+        cameraMap.Focus(focusPoint);
+
+        isFocused = true;
+    }
+
+    public void ExitFocus()
+    {
+        cameraMap.Back();
+
+        isFocused = false;
+    }
+}
