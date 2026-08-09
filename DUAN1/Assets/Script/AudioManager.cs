2c2dc4c605719b323df45f4bb3fca01594b73fce
Hoang Nam <nam868115@gmail.com>
2026-08-09 19:45:27 +0700

    hoan thanh dangky dangnhap setting

---
+++ DUAN1/Assets/Script/AudioManager.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance;
+
+    public AudioSource musicSource;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        musicSource.volume = volume;
+        PlayerPrefs.SetFloat("MusicVolume", volume);
+        PlayerPrefs.Save();
+    }
+
+    public float GetMusicVolume()
+    {
+        return PlayerPrefs.GetFloat("MusicVolume", 1f);
+    }
+}
