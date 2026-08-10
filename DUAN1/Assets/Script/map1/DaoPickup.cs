ae52e4875f2516aee62903597c19951b54ca6b48
Tsuna1357 <tsuna1357911@gmail.com>
2026-08-10 07:19:40 +0700

    update sth

--- DUAN1/Assets/Script/map1/DaoPickup.cs
+++ DUAN1/Assets/Script/map1/DaoPickup.cs
@@ -2,21 +2,39 @@ using UnityEngine;
 
 public class DaoPickup : Interactable
 {
+    [Header("Cabinet")]
     public Cabinet cabinet;
 
+    [Tooltip("Bật nếu item nằm trong cửa trái, tắt nếu item nằm trong cửa phải.")]
+    public bool isLeftDoor = true;
+
+    [Header("Item")]
     public ItemData itemData;
 
     public override void OnClick()
     {
-        if (!cabinet.leftDoorOpen)
+        // Kiểm tra cửa tương ứng đã mở chưa
+        if (isLeftDoor)
         {
-            Debug.Log("Phải mở cửa trái trước.");
-
-            return;
+            if (!cabinet.leftDoorOpen)
+            {
+                Debug.Log("Phải mở cửa trái trước.");
+                return;
+            }
+        }
+        else
+        {
+            if (!cabinet.rightDoorOpen)
+            {
+                Debug.Log("Phải mở cửa phải trước.");
+                return;
+            }
         }
 
+        // Thêm item vào inventory
         if (InventoryManager.Instance.AddItem(itemData))
         {
+            // Nhặt thành công thì tắt object
             gameObject.SetActive(false);
         }
     }
